08d89f671008c422387c3de5e149e6421d7d45d2
Audie Milson <milson.audie@gmail.com>
2023-08-12 21:45:48 +0700

    Add config to not require importing vitest functions

--- js-unit-test/tests/sample.test.js
+++ js-unit-test/tests/sample.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+// import { describe, expect, it } from "vitest";
 
 /**
  * npx vitest -> run all test file and wait for changes
--- js-unit-test/vitest.config.js
+++ js-unit-test/vitest.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from "vitest/dist/config";
 export default defineConfig({
   test: {
     dir: "tests", // only run test file in this folder
+    globals: true, // to run vitest functions without importing
   },
 });
