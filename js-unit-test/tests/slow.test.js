70aa11fac2c66e02a250b62bf016e126dc4d58e8
Audie Milson <milson.audie@gmail.com>
2023-08-12 21:50:39 +0700

    Vitest Multi thread

---
+++ js-unit-test/tests/slow.test.js
@@ -0,0 +1,18 @@
+/**
+ * concurrent -> to run multi thread
+ */
+
+describe("slow test", () => {
+  it.concurrent("should pass", async () => {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    expect(1).toBe(1);
+  });
+  it.concurrent("should pass", async () => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    expect(1).toBe(1);
+  });
+  it.concurrent("should pass", async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    expect(1).toBe(1);
+  });
+});
