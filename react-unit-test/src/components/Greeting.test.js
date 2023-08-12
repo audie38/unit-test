1f61dd14852686438a5d51b07beb304fa92555cf
Audie Milson <milson.audie@gmail.com>
2023-08-13 00:05:08 +0700

    React Unit Test, testing user interaction and state

--- react-unit-test/src/components/Greeting.test.js
+++ react-unit-test/src/components/Greeting.test.js
@@ -1,13 +1,44 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-test("renders Hello World", () => {
-  // Arrange
-  render(<Greeting />);
+describe("Greeting Component", () => {
+  test("renders Hello World", () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act
+    // Act
 
-  // Assert
-  const helloWorldEl = screen.getByText("Hello World", { exact: true });
-  expect(helloWorldEl).toBeInTheDocument();
+    // Assert
+    const helloWorldEl = screen.getByText("Hello World", { exact: false });
+    expect(helloWorldEl).toBeInTheDocument();
+  });
+  test("renders good to see you if the button was not clicked", () => {
+    // Arrange
+    render(<Greeting />);
+    // Assert
+    const paragraphEl = screen.getByText("good to see you", { exact: false });
+    expect(paragraphEl).toBeInTheDocument();
+  });
+  test("renders changed if the button was clicked", async () => {
+    // Arrange
+    render(<Greeting />);
+    // Act
+    const buttonEl = screen.getByRole("button");
+    await userEvent.click(buttonEl);
+    // Assert
+    const paragraphEl = screen.getByText("Changed", { exact: false });
+    expect(paragraphEl).toBeInTheDocument();
+  });
+  test("renders good to see you if ONLY the button was NOT clicked", async () => {
+    // Arrange
+    render(<Greeting />);
+    // Act
+    const buttonEl = screen.getByRole("button");
+    await userEvent.click(buttonEl);
+    // Assert
+    const paragraphEl = screen.queryByText("good to see you", { exact: false }); // queryByText will return null if not found, getByText will throw an error if not found
+    // expect(paragraphEl).not.toBeInTheDocument();
+    expect(paragraphEl).toBeNull();
+  });
 });
